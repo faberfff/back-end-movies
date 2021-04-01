74ed805f877e24f5abf0c8f9d84bd41337676b6a
faber9090 <faber.fernadez@est.iudigital.edu.co>
2021-04-01 15:07:48 -0500

    primer commit del back-end del proyecto movies

---
+++ src/main/java/co/edu/iudigital/model/repository/UsersRepository.java
@@ -0,0 +1,9 @@
+package co.edu.iudigital.model.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import co.edu.iudigital.model.entity.Users;
+
+public interface UsersRepository extends CrudRepository<Users, Integer> {
+
+}
