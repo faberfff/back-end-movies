f5581684b34759739ee75a0df8dc410a0e8a5440
faber9090 <faber.fernadez@est.iudigital.edu.co>
2021-04-01 15:27:39 -0500

    comentario de prueba

--- src/main/java/co/edu/iudigital/FilmsSpringbootAppApplication.java
+++ src/main/java/co/edu/iudigital/FilmsSpringbootAppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FilmsSpringbootAppApplication {
-
+      //metodo main 
 	public static void main(String[] args) {
 		SpringApplication.run(FilmsSpringbootAppApplication.class, args);
 	}
